f249bff3ce19cb2c3a4851c5632e8543d15ff5b9
John Gleason <johnnygtech@users.noreply.github.com>
2025-06-27 23:06:54 -0400

    add sync flair, update leaderboard to use flair table, update trade_flair on leaderboard

---
+++ sync-flair.ps1
@@ -0,0 +1,3 @@
+$threads = Invoke-SqlQuery -Query "select * from tradethreads" -ConnectionName redditbot
+$spaceUsers = Invoke-SqlQuery -Query "select * from messages join users on messages.userId = users.id where tradeThreadId = 4" -ConnectionName redditbot                                                                                             
+$spaceusers.redditId1 | select -Unique | %{$currentUser=$_;$UserFlair = Invoke-SqlQuery -query "select * from flair where userId = (select id from users where redditId = @user)" -Parameters @{user=$currentUser} -ConnectionName redditbot;$userFlair; python3 .\Set-UserFlair.py $threads[3].subreddit $currentUser "Trades: $($userflair.trade_count):upvote:" }  
--- update-leaderboard.ps1
+++ update-leaderboard.ps1
@@ -2,6 +2,6 @@
 $credential = $(New-Object -TypeName 'System.Management.Automation.PsCredential' -ArgumentList $ENV:MYSQL_USER,$ss)
 Open-mySqlConnection -ConnectionName redditbot -Server $ENV:MYSQL_SERVER -Port $ENV:MYSQL_SERVER_PORT -Database redditbot -credential $credential
 
-$jsonTradeFlair = invoke-sqlquery -query "select JSON_OBJECTAGG(Username,TradeFlair) From calculatedTradeFlair" -ConnectionName redditbot
-$jsonTradeFlair.'JSON_OBJECTAGG(Username,TradeFlair)' | set-content -Path ./tradeCountDisplay/trade_flair.json
+$jsonTradeFlair = invoke-sqlquery -query "SELECT JSON_OBJECTAGG(users.redditId, flair.trade_count) AS TradeFlair FROM flair JOIN users ON flair.userid = users.id" -ConnectionName redditbot
+$jsonTradeFlair.'TradeFlair' | set-content -Path ./tradeCountDisplay/trade_flair.json
 aws s3 cp ./tradeCountDisplay/trade_flair.json s3://ccautoflair.bitbinder.net/trade_flair.json
