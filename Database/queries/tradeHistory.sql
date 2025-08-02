b9f73aa6628ada7e889d0d8efebb0a0c9410e557
John Gleason <johnnygtech@users.noreply.github.com>
2025-08-01 23:28:56 -0400

    fix api trade history query to include all subs

---
+++ Database/queries/tradeHistory.sql
@@ -0,0 +1,27 @@
+            WITH
+            confirmations as (
+            SELECT messages.redditId as MessageId,redditParentId as ParentMessageId,LOWER(body) LIKE '%confirmed%',body,created, users.redditId as Username FROM messages
+            JOIN users on messages.userId = users.id
+            where tradeThreadId in (select id from tradethreads where active = 1) and LOWER(body) LIKE '%confirmed%' and (redditParentId is not null and redditParentId <> '')),
+            top_level_comments as (
+            SELECT messages.redditId as MessageId,redditParentId as ParentMessageId,body,created, users.id as userID ,users.redditId as Username
+            FROM messages
+            JOIN users on messages.userId = users.id
+            where tradeThreadId in (select id from tradethreads where active = 1) and (redditParentId is null or redditParentId = '')),
+            trade_log as (
+            select t.MessageId as topLevelMessage, c.MessageId as confirmingMessage, t.Username as commentingUser, c.Username as taggedUser, t.body, c.body as confirmation
+            FROM confirmations c
+            JOIN top_level_comments t on c.ParentMessageId = t.MessageId
+            where t.Username <> c.Username AND t.body LIKE CONCAT('%u/', c.Username, '%')
+            )
+
+            select m2.created, commentingUser, taggedUser, m2.body as trade, m1.body as confirmation, CONCAT((select url from tradethreads where id = m2.tradeThreadId),m2.redditId,"/") as link from trade_log 
+            join messages m1 on trade_log.confirmingMessage = m1.redditId
+            join messages m2 on trade_log.topLevelMessage = m2.redditId
+            join tradethreads on m2.tradeThreadId = tradethreads.id
+            where 
+commentingUser = 'Lucky_Cold9500' or taggedUser = 'Lucky_Cold9500';
+
+
+select * from users where redditid = 'Lucky_Cold9500'
+select * from messages where userid = 162119
--- api/api.ps1
+++ api/api.ps1
@@ -52,12 +52,12 @@ Start-PodeServer {
             confirmations as (
             SELECT messages.redditId as MessageId,redditParentId as ParentMessageId,LOWER(body) LIKE '%confirmed%',body,created, users.redditId as Username FROM messages
             JOIN users on messages.userId = users.id
-            where tradeThreadId = 2 and LOWER(body) LIKE '%confirmed%' and (redditParentId is not null and redditParentId <> '')),
+            where tradeThreadId in (select id from tradethreads where active = 1) and LOWER(body) LIKE '%confirmed%' and (redditParentId is not null and redditParentId <> '')),
             top_level_comments as (
             SELECT messages.redditId as MessageId,redditParentId as ParentMessageId,body,created, users.id as userID ,users.redditId as Username
             FROM messages
             JOIN users on messages.userId = users.id
-            where tradeThreadId = 2 and (redditParentId is null or redditParentId = '')),
+            where tradeThreadId in (select id from tradethreads where active = 1) and (redditParentId is null or redditParentId = '')),
             trade_log as (
             select t.MessageId as topLevelMessage, c.MessageId as confirmingMessage, t.Username as commentingUser, c.Username as taggedUser, t.body, c.body as confirmation
             FROM confirmations c
@@ -124,7 +124,7 @@ Start-PodeServer {
             $dict[$row.redditId] = [int]$row.trade_count
         }
         Write-PodeJsonResponse -Value $dict
-        
+
         close-sqlconnection -ConnectionName $using:connectionName
     }
 
