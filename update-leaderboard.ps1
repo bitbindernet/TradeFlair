5430cf788b290c3a12196df0672f9e76d79c1b0f
John Gleason <johnnygtech@users.noreply.github.com>
2025-08-01 22:04:50 -0400

    add tradehistory and update index

--- update-leaderboard.ps1
+++ update-leaderboard.ps1
@@ -1,8 +1,9 @@
 . ./awsHelper.ps1 -prod
-$credential = $(New-Object -TypeName 'System.Management.Automation.PsCredential' -ArgumentList $ENV:MYSQL_USER,$ss)
-Open-mySqlConnection -ConnectionName redditbot -Server $ENV:MYSQL_SERVER -Port $ENV:MYSQL_SERVER_PORT -Database redditbot -credential $credential
+#$credential = $(New-Object -TypeName 'System.Management.Automation.PsCredential' -ArgumentList $ENV:MYSQL_USER,$ss)
+#Open-mySqlConnection -ConnectionName redditbot -Server $ENV:MYSQL_SERVER -Port $ENV:MYSQL_SERVER_PORT -Database redditbot -credential $credential
 
-$jsonTradeFlair = invoke-sqlquery -query "SELECT JSON_OBJECTAGG(users.redditId, flair.trade_count) AS TradeFlair FROM flair JOIN users ON flair.userid = users.id" -ConnectionName redditbot
-$jsonTradeFlair.'TradeFlair' | set-content -Path ./tradeCountDisplay/trade_flair.json
-aws s3 cp ./tradeCountDisplay/trade_flair.json s3://ccautoflair.bitbinder.net/trade_flair.json
+#$jsonTradeFlair = invoke-sqlquery -query "SELECT JSON_OBJECTAGG(users.redditId, flair.trade_count) AS TradeFlair FROM flair JOIN users ON flair.userid = users.id" -ConnectionName redditbot
+#$jsonTradeFlair.'TradeFlair' | set-content -Path ./tradeCountDisplay/trade_flair.json
+#aws s3 cp ./tradeCountDisplay/trade_flair.json s3://ccautoflair.bitbinder.net/trade_flair.json
 aws s3 cp ./tradeCountDisplay/index.html s3://ccautoflair.bitbinder.net/index.html
+aws s3 cp ./tradeCountDisplay/tradehistory.html s3://ccautoflair.bitbinder.net/tradehistory.html
