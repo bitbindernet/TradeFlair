88b0aea10bb3867c8cf777b4a8ffba51ef921c7a
John Gleason <johnnygtech@users.noreply.github.com>
2025-08-01 22:50:49 -0400

    only sync active threads

--- runSync.ps1
+++ runSync.ps1
@@ -9,7 +9,7 @@ $epoch3days = [int][double]::Parse(
 $ss = ConvertTo-SecureString $ENV:MYSQL_PASSWORD -AsPlainText -Force
 Open-mySqlConnection -ConnectionName redditbot -Server $ENV:MYSQL_SERVER -Port $ENV:MYSQL_SERVER_PORT -Database redditbot -credential $(New-Object -TypeName 'System.Management.Automation.PsCredential' -ArgumentList $ENV:MYSQL_USER,$ss)
 
-$threads = Invoke-SqlQuery -Query "select * from tradethreads" -ConnectionName redditbot
+$threads = Invoke-SqlQuery -Query "select * from tradethreads where active = 1" -ConnectionName redditbot
 
 $threads | %{
      #$ENV:REDDIT_ENV         = 
